39ebe3b708411434f299119f0a42b1dadd9c1e5e
Aakash Kumar <122554143+aakash-6205@users.noreply.github.com>
2024-06-08 22:00:47 +0530

    Add files via upload

---
+++ lower_trangle.py
@@ -0,0 +1,5 @@
+#Lower Triangular Pattern python
+n =int(input("Enter the value of n: "))
+for i in range(1, n + 1):
+    print("* " * i)
+print("\n")
---
+++ upper_triangle.py
@@ -0,0 +1,5 @@
+#Upper Triangular Pattern
+n =int(input("Enter the value of n: "))
+for i in range(n, 0, -1):
+    print("* " * i)
+print("\n")
